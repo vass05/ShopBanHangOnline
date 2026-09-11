38527f41b0e244f93a2a2f641fa6e2901f53257b
vietanh1205 <vuvietanh12052005@gmail.com>
2026-09-11 11:09:55 +0700

    refactor(auth): remove unused Email import in LoginRequest

--- backend/src/main/java/com/helishop/core/modules/auth/dto/LoginRequest.java
+++ backend/src/main/java/com/helishop/core/modules/auth/dto/LoginRequest.java
@@ -1,7 +1,6 @@
 package com.helishop.core.modules.auth.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
